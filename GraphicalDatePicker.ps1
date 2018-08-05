26f6773affdf248e22273799bf4e96d530f07db9
Sagar V Hande <sagarvhande@gmail.com>
2018-08-05 15:06:42 +0530

    GraphicalDatePicker
    Graphical Date Picker

---
+++ GraphicalDatePicker.ps1
@@ -0,0 +1,40 @@
+
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object Windows.Forms.Form
+
+$form.Text = 'Select a Date'
+$form.Size = New-Object Drawing.Size @(243,230)
+$form.StartPosition = 'CenterScreen'
+
+$calendar = New-Object System.Windows.Forms.MonthCalendar
+$calendar.ShowTodayCircle = $false
+$calendar.MaxSelectionCount = 1
+$form.Controls.Add($calendar)
+
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Point(38,165)
+$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton.Text = 'OK'
+$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $OKButton
+$form.Controls.Add($OKButton)
+
+$CancelButton = New-Object System.Windows.Forms.Button
+$CancelButton.Location = New-Object System.Drawing.Point(113,165)
+$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+$CancelButton.Text = 'Cancel'
+$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $CancelButton
+$form.Controls.Add($CancelButton)
+
+$form.Topmost = $true
+
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $date = $calendar.SelectionStart
+    Write-Host "Date selected: $($date.ToShortDateString())"
+}
